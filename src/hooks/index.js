89bb5b20c2735880a9e3e48f045b9b61ff78cdd8
Abhijit Samanta <samantaabhijit354@gmail.com>
2026-07-05 17:40:35 +0530

    Add index.js to export custom hooks

---
+++ src/hooks/index.js
@@ -0,0 +1,4 @@
+export { useMovies } from './useMovies';
+export { useMovieDetails } from './useMovieDetails';
+export { useWatchlist } from './useWatchlist';
+export { useFavorites } from './useFavorites';
